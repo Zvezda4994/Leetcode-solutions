7f1e873411f1cc16841fea85f1c36aed6022d035
Aditya Singh <adityaviveksingh@gmail.com>
2025-01-02 17:45:30 -0400

    updated EncodeAndDecodeStrings optimal sol

--- Arrays and Hashing/EncodeAndDecodeStrings.py
+++ Arrays and Hashing/EncodeAndDecodeStrings.py
@@ -1,31 +1,23 @@
 class Solution:
+    
     def encode(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        sizes, res = [], ""
-        for s in strs:
-            sizes.append(len(s))
-        for sz in sizes:
-            res += str(sz)
-            res += ','
-        res += '#'
-        for s in strs:
-            res += s
+        res = "" #string to store encoded result
+        for s in strs: #for each string in the input, add it to res with a delimiter and length
+            res += str(len(s)) + "#" + s
         return res
 
     def decode(self, s: str) -> List[str]:
-        if not s:
-            return []
-        sizes, res, i = [], [], 0
-        while s[i] != '#':
-            cur = ""
-            while s[i] != ',':
-                cur += s[i]
-                i += 1
-            sizes.append(int(cur))
-            i += 1
-        i += 1
-        for sz in sizes:
-            res.append(s[i:i + sz])
-            i += sz
+        res = [] #empty list to store decoded strings
+        i = 0 #traversal index
+        
+        while i < len(s): #loop through string until the end
+            j = i
+            while s[j] != '#': #j starts at current index until the delimiter
+                j += 1
+            length = int(s[i:j]) #covert length of this word to int
+            i = j + 1 #update i to start of the string (past the delimiter)
+            j = i + length #endpoint of the string
+            res.append(s[i:j]) #append extracted string to the result list
+            i = j #start processing next length
+             
         return res
